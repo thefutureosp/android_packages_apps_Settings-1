1813ec67475967cbe70e578af693bf8cd6cec20b
Orca Project <shamukillaz@orcaproject.co>
2013-09-05 19:43:05 -0400

    Settings: Add PagerTabStrip indicator color

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 PAC-man ROM
+ * Modified By The Orca Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +17,6 @@
 
 package com.android.settings;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.app.LocalActivityManager;
 import android.content.Intent;
@@ -31,6 +29,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.DisplayMetrics;
@@ -44,19 +43,23 @@ import android.view.LayoutInflater;
 import android.util.ExtendedPropertiesUtils;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Settings extends Activity {
 
-    private ViewPager mPager;
-    private List<View> listViews;
-    private TextView pac_tab, system_tab;
-    private int offset = 0;
-    private int currIndex = 0;
-    private int bmpW;
-    private LocalActivityManager localManager;
+    private static final String[] titles = { "System", "Orca Settings" };
     private LayoutInflater mInflater;
+    private List<View> listViews;
+    private LocalActivityManager localManager;
+    private PagerTabStrip mTabStrip;
+    private TextView pac_tab, system_tab;
+    private ViewPager mPager;
     private ViewPagerAdapter mPagerAdapter;
-    private static final String[] titles = { "System", "ORCA" };
+    private int bmpW;
+    private int currIndex = 0;
     private int mLayout = 0;
+    private int offset = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -95,6 +98,8 @@ public class Settings extends Activity {
         mPager.setAdapter(mPagerAdapter);
         mPager.setCurrentItem(0);
         mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+        mTabStrip = (PagerTabStrip) findViewById(R.id.pager_title_strip);
+        mTabStrip.setTabIndicatorColorResource(android.R.color.holo_blue_light);
     }
 
     public class ViewPagerAdapter extends PagerAdapter {
